f6e4c6fd1947312977bfc3e9700ba9c921902e63
Ed <105404937+indy6678@users.noreply.github.com>
2022-10-24 21:28:02 -0500

    added user model

---
+++ server/models/User.js
@@ -0,0 +1,46 @@
+const { Schema, model } = require('mongoose')
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true
+        },
+
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/.+@.+\..+/, 'Must match a valid email address!']
+        },
+
+        password: {
+            type: String,
+            required: true,
+            minlength: 7
+        },
+
+        posts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Post'
+            }
+        ],
+
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: { virtuals: true}
+    }
+);
+
+const User = model('User', userSchema)
+
+module.exports = User;
