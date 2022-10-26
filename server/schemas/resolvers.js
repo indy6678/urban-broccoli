b8d0c25c692e9afd0bc2f41f48c22c095ac76bf5
Ed <105404937+indy6678@users.noreply.github.com>
2022-10-25 22:36:36 -0500

    added resolvers.js and index.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,12 @@
 const {User, Post} = require('../models');
 
 const resolvers = {
-    
+    Query: {
+        users: async () => {
+            return User.find()
+            .select('-__v -password')
+        }
+    }
 }
+
+module.exports = resolvers;
