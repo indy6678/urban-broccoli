8966ffafdf8e1d97184206728e37732093849b20
Ed <105404937+indy6678@users.noreply.github.com>
2022-10-25 21:13:57 -0500

    added reply

---
+++ server/models/Reply.js
@@ -0,0 +1,21 @@
+const {Schema} = require('mongoose');
+
+const replySchema = new Schema(
+    {
+        replyBody: {
+            type: String,
+            required: true,
+            maxlength: 255
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        }
+    }
+);
+
+module.exports = replySchema;
