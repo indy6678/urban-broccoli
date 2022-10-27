468be99dd806396b64895568404c064c14194be7
Ed <105404937+indy6678@users.noreply.github.com>
2022-10-26 19:17:57 -0500

    added seed

---
+++ server/seeds/seeds.js
@@ -0,0 +1,71 @@
+// import faker
+const faker = require('faker');
+
+// import db
+const db = require('../config/connection');
+// import models
+const { Post, User } = require('../models');
+
+db.once('open', async () => {
+    await Post.deleteMany({});
+    await User.deleteMany({});
+
+    // create users
+    const userData = [];
+
+    for (let i = 0; i < 10; i++) {
+        const username = faker.internet.userName()
+        const email = faker.internet.email(username)
+        const password = faker.internet.password()
+
+        // add username, email, and password to userData array
+        userData.push({ username, email, password })
+    }
+
+    const createdUsers = await User.collection.insertMany(userData)
+
+    // create posts
+    let createdPosts = []
+    for (let i = 0; i < 5; i++) {
+        // create post of random words with length between 5-15
+        const postText = faker.lorem.words(Math.floor(Math.random() * 10) + 5)
+
+        // returns the index of a user randomly
+        const randomUserIndex = Math.floor(Math.random() * createdUsers.ops.length)
+        // destructure username and _id from user at index of randomUserIndex
+        const { username, _id: userid } = createdUsers.ops[randomUserIndex]
+        // create a post with created text and username
+        const createdPost = await Post.create({ postText, username })
+        // update a user
+        const updatedUser = await User.updateOne(
+            { _id: userId }, { $push: { posts: createdPost._id } }
+        )
+        //add createdPost to createdPosts array
+        createdPosts.push(createdPost);
+    }
+
+    // create replies
+    for (let i = 0; i < 5; i++) {
+        // create reply of random words with length between 5-15
+        const replyBody = faker.lorem.words(Math.floor(Math.random() * 10) + 5)
+        // returns the index of a user randomly
+        const randomUserIndex = Math.floor(Math.random() * createdUsers.ops.length)
+        // destructure username and _id from selected user
+        const { username, _id: userid } = createdUsers.ops[randomUserIndex]
+        // return random number between 0 and length of createdPosts array and saves in randomPostIndex
+        const randomPostIndex = Math.floor(Math.random() * createdPosts.length)
+        // destructure _id from post found at randomPostIndex value in createdPosts array
+        const { _id: thoughtId } = createdPosts[randomPostIndex]
+
+        // update a Post with the reply
+        await Post.updateOne(
+            { _id: thoughtId },
+            { $push: { replies: { replyBody, username } } },
+            { runValidators: true }
+        )
+
+    }
+
+    console.log('Seeding complete!')
+    process.exit(0)
+})
