5505bbc570c0cda786e52e6ee16dd39107f3528d
Ed <105404937+indy6678@users.noreply.github.com>
2022-10-28 22:43:25 -0500

    added addpost mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -56,7 +56,20 @@ const resolvers = {
             const token = signToken(user);
             return { token, user };
         },
-        
+        addPost: async (parent, args, context) => {
+            // check to see if the user is logged in
+            if (context.user) {
+                const post = await Post.create({ ...args, username: context.user.username });
+
+                await User.findByIdAndUpdate(
+                    { _id: context.user._id },
+                    { $push: { posts: post._id } },
+                    { new: true }
+                );
+                return post;
+            }
+            throw new AuthenticationError('You need to be logged in to add a post!');
+        }
     }
 }
 
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -35,6 +35,8 @@ const typeDefs = gql`
     type Mutation {
         login(email: String!, password: String!): Auth
         addUser(username: String!, email: String!, password: String!): Auth
+        addPost(postText: String!): Post
+        addReply(postId: ID!, replyBody: String!): Post
     }
     type Auth {
         token: ID!
