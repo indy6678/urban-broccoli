9269d92e51a550e46c7100299d125e9cfbb3b566
Ed <105404937+indy6678@users.noreply.github.com>
2022-10-29 14:45:18 -0500

    Revert "installed react, added apollo client and graphql"
    This reverts commit d4a1716c069bebe5113993414f64775283d5a41c.

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
 
-// ApolloProvider provides data to all other componenets
-// Client is constructor to help initialize conn to API server
-// InMemoryCache enables Apollo Client instance to cache API response data for better efficiency
-// createHttpLink allows control over how the Apollo Client makes a request (like middleware)
-import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink} from '@apollo/client';
-import { application } from 'express';
-
-// establish new link to GQL server at /graphql endpoint
-const httpLink = createHttpLink({
-  uri: 'http://localhost:3001/graphql',
-});
-
-// instantiate apollo client link and  create connection to API endpoint/
-const client = new ApolloClient({
-  link: httpLink,
-  cache: new InMemoryCache(),
-});
-
 function App() {
   return (
     <div className="App">
