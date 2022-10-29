5c39703757e0c4fb6972c8952e05b2007750f501
Ed <105404937+indy6678@users.noreply.github.com>
2022-10-29 17:22:29 -0500

    added initial header

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
 import Home from './pages/Home';
+import Header from './components/Header';
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 
 const httpLink = createHttpLink({
@@ -16,8 +16,11 @@ function App() {
   return (
     // wrap JSX code with Apolloprovider to allow everything to have access to server's API data through client
     <ApolloProvider client={client}>
-      <div>
-        <Home />
+      <div className='flex-column justify-flex-start min-10-vh'>
+        <Header />
+        <div>
+          <Home />
+        </div>
       </div>
     </ApolloProvider>
   )
