a232722c8bf7e7d5ffca966a3c31af0c24f1f10a
Ed <105404937+indy6678@users.noreply.github.com>
2022-10-29 20:20:37 -0500

    added home and singlepost routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Home from './pages/Home';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import SinglePost from './pages/SinglePost'
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 // import from react-router-dom, renaming browserrouter as router
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -19,13 +20,24 @@ function App() {
   return (
     // wrap JSX code with Apolloprovider to allow everything to have access to server's API data through client
     <ApolloProvider client={client}>
+      <Router>
       <div className='flex-column justify-flex-start min-10-vh'>
         <Header />
-        <div>
-          <Home />
+        <div className='container'>
+          <Routes>
+            <Route
+            path='/'
+            element={<Home />}
+            />
+            <Route
+            path='/post'
+            element={<SinglePost />}
+            />
+            </Routes>
         </div>
         <Footer/>
       </div>
+      </Router>
     </ApolloProvider>
   )
 }
