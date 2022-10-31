3a73c9529f07a3d2d32a6ebf71c8fa1b10ff0629
Ed <105404937+indy6678@users.noreply.github.com>
2022-10-31 16:28:43 -0500

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/urban-broccoli', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/urban-broccoli', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     // use the below with v5.9.9 or lower
