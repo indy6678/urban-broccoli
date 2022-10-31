a7cf726baa81a4c35233e7c0a6c64c0232a17d00
Ed <105404937+indy6678@users.noreply.github.com>
2022-10-31 11:20:43 -0500

    added replyform

---
+++ client/src/components/ReplyForm/index.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+const ReplyForm = ({ postId }) => {
+  return (
+    <div>
+      <form className="flex-row justify-center justify-space-between-md align-stretch">
+        <textarea
+          placeholder="Leave a reply..."
+          value={replyText}
+          className="form-input col-12 col-md-9"
+        ></textarea>
+        <button className="btn col-12 col-md-3" type="submit">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default ReplyForm;
